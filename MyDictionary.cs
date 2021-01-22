68de0d919c402e0d232c7dc72612da29c39b0965
Dorukhan Cosgun <66283837+dorukhancosgun@users.noreply.github.com>
2021-01-22 21:29:59 +0100

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T1,T2>
+    {
+        T1[] _keys;
+        T2[] _values;
+
+        public MyDictionary()
+        {
+
+            _keys = new T1[0];
+            _values = new T2[00];
+
+        }
+
+        public void Add(T1 item1, T2 item2)
+        {
+
+            T1[] tempArray = _keys;
+            T2[] tempArray2 = _values;
+
+            _keys = new T1[_keys.Length + 1];
+            _values = new T2[_values.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                _keys[i] = tempArray[i];
+            }
+
+            _keys[_keys.Length - 1] = item1;
+
+            for (int j = 0; j < tempArray2.Length; j++)
+            {
+                _values[j] = tempArray2[j];
+
+            }
+            _values[_values.Length - 1] = item2;
+        }
+
+        public int Count
+        {
+            get { return _keys.Length; }
+        }
+
+        public T1 Keys
+        {
+            get { return _keys[_keys.Length - 1]; }
+        }
+        public T2 Values
+        {
+            get { return _values[_values.Length - 1]; }
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> ustadlar = new MyDictionary<int, string>();
+
+            ustadlar.Add(155, "Friedrich Nietzsche");
+            ustadlar.Add(186, "Ivan Pavlov");
+            ustadlar.Add(112, "Sigmund Freud");
+            ustadlar.Add(110, "Arthur Schopehauer");
+
+            Console.WriteLine("Numarasi: " + ustadlar.Count);
+            Console.WriteLine(ustadlar.Values);
+            Console.WriteLine(ustadlar.Keys);
+        }
+    }
+}
